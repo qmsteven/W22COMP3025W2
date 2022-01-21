e2ed796a7e4a49aae756b8685574e72094d024c0
qmsteven <74738665+qmsteven@users.noreply.github.com>
2022-01-21 10:46:13 -0500

    Lesson end

--- app/src/main/java/com/lh1145112w1/w22comp3025w2/MainActivity.kt
+++ app/src/main/java/com/lh1145112w1/w22comp3025w2/MainActivity.kt
@@ -2,6 +2,10 @@ package com.lh1145112w1.w22comp3025w2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.android.material.snackbar.Snackbar
 import com.lh1145112w1.w22comp3025w2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toastButton.setOnClickListener {
+            Toast.makeText(this, "This is a Toast", Toast.LENGTH_LONG).show()
+        }
+
+        binding.snackBarButton.setOnClickListener {
+            Snackbar.make(binding.layout, "Snackbar message", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.snackBarWithActionButton.setOnClickListener {
+            val snackbar = Snackbar.make(binding.layout, "Should we toggle the image?", Snackbar.LENGTH_INDEFINITE)
+            snackbar.setAction("Click Here", View.OnClickListener {
+                binding.imageView.isVisible = !binding.imageView.isVisible
+            })
+            snackbar.show()
+        }
     }
 }
